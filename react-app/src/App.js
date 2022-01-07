873c1233981592b5efdfeb0289a03d7a1245094b
SeoDongSu <poopoo0515@naver.com>
2022-01-08 01:50:47 +0900

    2022.01.07

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,35 +1,61 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, settoDo] = useState("");
-  const [toDos, settoDos] = useState([]);
-  const onChange = (event) => settoDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    settoDo("");
-    settoDos((CurrentArray) => [toDo, ...toDos]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  // console.log(coins);
+  const [value, setvalue] = useState(0);
+  const [values, setvalues] = useState("0");
+  // const onChange = (item) => {
+  //   item.preventDefualt();
+  //   setvalue(item);
+  // };
+  const onChange = (item) => {
+    setvalue(item.target.value);
+  };
+  // const onSubmit = (item) => {
+  //   item.preventDefualt();
+  // };
+  const [symbol, setsymbol] = useState("%");
+
+  const onSelect = (item) => {
+    console.log(item);
+    // item.preventDefualt();
+    setsymbol(item.target.value.symbol);
+    setvalues(item.target.value.quotes.USD.price);
+    // console.log(item.target.value.quotes.USD.price);
   };
   return (
     <div>
-      <h1>{toDos}</h1>
-      <form onSubmit={onSubmit}>
+      <h1>The Coins!</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <form>
         <input
-          onChange={onChange}
-          text=""
           type="text"
-          placeholder="Write here"
+          placeholder="Write your dollar..."
+          onChange={onChange}
         ></input>
-        <button>Submit</button>
+        <select onChange={onSelect}>
+          {coins.map((item, index) => (
+            <option key={index}>
+              {item.name}: {value / item.quotes.USD.price}개
+            </option>
+          ))}
+        </select>
       </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h3>{value}$</h3>
+      <h3>
+        {values}
+        {symbol}
+      </h3>
     </div>
   );
 }
