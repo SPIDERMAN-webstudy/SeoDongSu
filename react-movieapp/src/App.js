3fafebf1aa1e34aaab4fd5328a2cb723497c9085
SeoDongSu <poopoo0515@naver.com>
2022-01-08 13:52:52 +0900

    refactoring

--- react-movieapp/src/App.js
+++ react-movieapp/src/App.js
@@ -5,13 +5,10 @@ import React from "react";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
-        <Route
-          path={process.env.PUBLIC_URL + "/movie/:id"}
-          element={<Detail />}
-        />
+        <Route path="/movie/:id" element={<Detail />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
