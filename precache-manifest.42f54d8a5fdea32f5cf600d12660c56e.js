88d36788babc07c4f472a14a37e1c70a30fdb913
SeoDongSu <poopoo0515@naver.com>
2022-01-08 12:58:42 +0900

    Updates

---
+++ precache-manifest.42f54d8a5fdea32f5cf600d12660c56e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b03a83c9025dcab2c1cd4d318e94179",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4da08c554d46b3cf539",
+    "url": "/static/js/2.b80722f8.chunk.js"
+  },
+  {
+    "revision": "dbd11ee13c65352ede3c",
+    "url": "/static/js/main.db611414.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
