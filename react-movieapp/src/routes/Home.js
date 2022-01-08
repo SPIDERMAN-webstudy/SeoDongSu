3d8b75ff27fa8de46bfeb748071aab8f22d50fa3
SeoDongSu <poopoo0515@naver.com>
2022-01-08 17:25:35 +0900

    2022.01.08-failproject

--- react-movieapp/src/routes/Home.js
+++ react-movieapp/src/routes/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import style from "../components/Movie.css";
 
 function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+  const [value, setvalue] = useState("");
+  const onChange = (event) => {
+    setvalue(event.target.value);
+  };
   const getMovies = async () => {
     const json = await (
       await fetch(
@@ -23,7 +28,35 @@ function Home() {
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <div>
+        <div className={style.movie}>
+          <div className="Top">
+            <div className="Title">
+              <h2>Dong's Movie Cinema</h2>
+            </div>
+            <div className="Search">
+              <form>
+                <input
+                  type="text"
+                  placeholder="장르,검색"
+                  onChange={onChange}
+                ></input>
+              </form>
+            </div>
+            <div className="TopMov"></div>
+            <div className="TopCon"></div>
+          </div>
+          <div className="TopRate">
+            <div className="TopRate_Title"></div>
+          </div>
+          <div className="genreSelec">
+            <div className="genreSelec_Title"></div>
+          </div>
+          <div className="JJim">
+            <div className="JJim_Title"></div>
+          </div>
+          <div className="Good">
+            <div className="Good_Title"></div>
+          </div>
           {movies.map((movie) => (
             <Movie
               key={movie.id}
